d25b39ef70900176e375abfca72bfb5b9e2e04c4
Silvia Senekowitsch <silvia.senekowitsch@greenmail.ch>
2022-07-16 19:47:55 +0200

    Create hello-world-server with axum #1

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router, extract::Path};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/:name", get(personal_greeting));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, Bünzli!</h1>")
+}
+
+async fn personal_greeting(Path(name): Path<String>) -> Html<String> {
+    Html(format!("<h1>Hello, {name}!</h1>"))
 }
