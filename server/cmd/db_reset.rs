d8e296a11f1522106805f2a1dca88b76e15d8270
Silvia Senekowitsch <silvia.senekowitsch@greenmail.ch>
2023-02-05 15:39:47 +0100

    rename db: fn new

--- server/cmd/db_reset.rs
+++ server/cmd/db_reset.rs
@@ -4,6 +4,6 @@ use lib::db;
 
 fn main() {
     std::fs::remove_dir_all(PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../dev/db")).ok();
-    let db = db::new();
+    let db = db::get();
     db::insert_seeds(db.as_ref());
 }
