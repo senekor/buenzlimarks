b0f6555a7b27a84a680373231bbedfe8d150c8e5
Silvia Senekowitsch <silvia.senekowitsch@greenmail.ch>
2023-02-12 15:06:00 +0100

    test(server): test error handling of handlers

--- server/src/handlers/bookmarks.rs
+++ server/src/handlers/bookmarks.rs
@@ -38,11 +38,9 @@ pub fn routes() -> Router<DB> {
 mod tests {
     use std::sync::Arc;
 
-    use mockall::predicate;
-
     use crate::{
-        db::MockDbTrait,
-        models::{bookmark::Bookmark, user::dev_user_id},
+        db::{MockDbTrait, error::DbError},
+        models::{bookmark::Bookmark},
     };
 
     use super::*;
@@ -60,12 +58,24 @@ mod tests {
         let expected = vec![bookmark.clone()];
 
         db.expect_get_bookmarks()
-            .with(predicate::eq(dev_user_id()))
             .returning(move |_| Ok(expected.clone()));
 
         let actual = get_bookmarks(User::dev(), State(Arc::new(db))).await;
 
         assert_eq!(actual.0, StatusCode::OK);
         assert_eq!(actual.1 .0, vec![bookmark]);
+    }
+
+    #[tokio::test]
+    async fn get_bookmarks_should_handle_db_error() {
+        let mut db = MockDbTrait::new();
+
+        db.expect_get_bookmarks()
+            .returning(move |_| Err(DbError::WhoopsieDoopsie));
+
+        let actual = get_bookmarks(User::dev(), State(Arc::new(db))).await;
+
+        assert_eq!(actual.0, StatusCode::INTERNAL_SERVER_ERROR);
+        assert_eq!(actual.1 .0, Vec::new());
     }
 }
