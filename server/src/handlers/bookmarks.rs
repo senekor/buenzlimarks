f1f1407c89f512100c6b06240cfd81a830871d35
Silvia Senekowitsch <silvia.senekowitsch@greenmail.ch>
2023-04-02 15:58:58 +0200

    improve readability

--- server/src/handlers/bookmarks.rs
+++ server/src/handlers/bookmarks.rs
@@ -36,27 +36,18 @@ pub async fn get_bookmarks(
     State(db): State<DB>,
     query: Option<Query<WidgetId>>,
 ) -> Result<Json<Vec<Bookmark>>, StatusCode> {
-    match query {
-        Some(w) => db
-            .get_bookmarks(&user.id)
-            .map(|v| {
-                Json(
-                    v.into_iter()
-                        .filter(|b| b.widget_id == w.widget_id)
-                        .collect::<Vec<Bookmark>>(),
-                )
-            })
-            .map_err(|e| match e {
-                DbError::NotFound => StatusCode::NOT_FOUND,
-                DbError::WhoopsieDoopsie => StatusCode::INTERNAL_SERVER_ERROR,
-                DbError::AlreadyExists => StatusCode::INTERNAL_SERVER_ERROR,
-            }),
-        None => db.get_bookmarks(&user.id).map(Json).map_err(|e| match e {
+    db.get_bookmarks(&user.id)
+        .map(|mut v| {
+            if let Some(w) = query {
+                v.retain(|b| b.widget_id == w.widget_id);
+            }
+            Json(v)
+        })
+        .map_err(|e| match e {
             DbError::NotFound => StatusCode::NOT_FOUND,
             DbError::WhoopsieDoopsie => StatusCode::INTERNAL_SERVER_ERROR,
             DbError::AlreadyExists => StatusCode::INTERNAL_SERVER_ERROR,
-        }),
-    }
+        })
 }
 
 pub async fn delete_bookmark(
@@ -72,68 +63,68 @@ pub async fn delete_bookmark(
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use std::sync::Arc;
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
 
-//     use crate::{
-//         db::{error::DbError, MockDbTrait},
-//         models::bookmark::Bookmark,
-//     };
+    use crate::{
+        db::{error::DbError, MockDbTrait},
+        models::bookmark::Bookmark,
+    };
 
-//     use super::*;
+    use super::*;
 
-//     #[tokio::test]
-//     async fn should_get_single_bookmark() {
-//         let mut db = MockDbTrait::new();
+    #[tokio::test]
+    async fn should_get_single_bookmark() {
+        let mut db = MockDbTrait::new();
 
-//         let bookmark = Bookmark {
-//             id: "0".into(),
-//             name: "name".into(),
-//             url: "url".into(),
-//             widget_id: "0".into(),
-//         };
-//         let expected = vec![bookmark.clone()];
+        let bookmark = Bookmark {
+            id: "0".into(),
+            name: "name".into(),
+            url: "url".into(),
+            widget_id: "0".into(),
+        };
+        let expected = vec![bookmark.clone()];
 
-//         db.expect_get_bookmarks()
-//             .returning(move |_| Ok(expected.clone()));
+        db.expect_get_bookmarks()
+            .returning(move |_| Ok(expected.clone()));
 
-//         let actual = get_bookmarks(User::dev(), State(Arc::new(db))).await;
+        let actual = get_bookmarks(User::dev(), State(Arc::new(db)), None).await;
 
-//         assert_eq!(actual.unwrap().0, vec![bookmark]);
-//     }
+        assert_eq!(actual.unwrap().0, vec![bookmark]);
+    }
 
-//     #[tokio::test]
-//     async fn get_bookmarks_should_handle_db_error() {
-//         let mut db = MockDbTrait::new();
+    #[tokio::test]
+    async fn get_bookmarks_should_handle_db_error() {
+        let mut db = MockDbTrait::new();
 
-//         db.expect_get_bookmarks()
-//             .returning(move |_| Err(DbError::WhoopsieDoopsie));
+        db.expect_get_bookmarks()
+            .returning(move |_| Err(DbError::WhoopsieDoopsie));
 
-//         let actual = get_bookmarks(User::dev(), State(Arc::new(db))).await;
+        let actual = get_bookmarks(User::dev(), State(Arc::new(db)), None).await;
 
-//         assert_eq!(actual.unwrap_err(), StatusCode::INTERNAL_SERVER_ERROR);
-//     }
+        assert_eq!(actual.unwrap_err(), StatusCode::INTERNAL_SERVER_ERROR);
+    }
 
-//     #[tokio::test]
-//     async fn should_create_bookmark() {
-//         let mut db = MockDbTrait::new();
+    #[tokio::test]
+    async fn should_create_bookmark() {
+        let mut db = MockDbTrait::new();
 
-//         let bookmark = Bookmark {
-//             id: "0".into(),
-//             name: "name".into(),
-//             url: "url".into(),
-//             widget_id: "0".into(),
-//         };
-//         let user_provided_id = bookmark.id.clone();
+        let bookmark = Bookmark {
+            id: "0".into(),
+            name: "name".into(),
+            url: "url".into(),
+            widget_id: "0".into(),
+        };
+        let user_provided_id = bookmark.id.clone();
 
-//         db.expect_insert_bookmark()
-//             .times(1)
-//             .withf(move |_, bm| bm.id != user_provided_id)
-//             .returning(move |_, _| Err(DbError::WhoopsieDoopsie));
+        db.expect_insert_bookmark()
+            .times(1)
+            .withf(move |_, bm| bm.id != user_provided_id)
+            .returning(move |_, _| Err(DbError::WhoopsieDoopsie));
 
-//         create_bookmark(User::dev(), State(Arc::new(db)), Json(bookmark))
-//             .await
-//             .ok();
-//     }
-// }
+        create_bookmark(User::dev(), State(Arc::new(db)), Json(bookmark))
+            .await
+            .ok();
+    }
+}
