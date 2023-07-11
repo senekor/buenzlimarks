053038a805a04e5df78f2e6279218dc75c1dbc67
Remo Senekowitsch <remo@buenzli.dev>
2023-07-12 01:54:42 +0200

    remove unused suspense

--- app/src/components/home.rs
+++ app/src/components/home.rs
@@ -25,48 +25,44 @@ pub fn Home(cx: Scope) -> impl IntoView {
     });
 
     view! { cx,
-        <Suspense
-            fallback=move || LoadingScreen(cx)
-        >
-            <div class="h-screen flex flex-col flex-wrap gap-2">
-                <div class="flex gap-2 p-2 w-full">
-                    <For
-                        each=move || pages.read(cx).unwrap_or_default()
-                        key=|page| page.id.clone()
-                        view=move |cx, page| {
-                            let id = store_value(cx, page.id.clone());
-                            let is_selected = Signal::derive(cx, move || {
-                                selected_page().is_some_and(|sp| sp.id == id())
-                            });
-                            let select = SignalSetter::map(cx, move |p| set_selected_page(Some(p)));
-                            view! { cx, <PageTab
-                                page
-                                is_selected
-                                select
-                                submit_page
-                                delete_page
-                            /> }
-                        }
-                    />
-                    <FlexSpace />
-                    <IconButton on:click=move |_| {
-                        submit_page.dispatch(PageType {
-                            id: "".into(),
-                            name: "new page".into(),
-                        })
-                    }>
-                        <PlusIcon />
-                    </IconButton>
-                    <IconButton on:click=move |_| auth.logout() >
-                        <ArrowRightOnRectangleIcon />
-                    </IconButton>
-                </div>
-                <Suspense fallback=move || "">{ move || {
-                    let sp = selected_page()?;
-                    let page = Signal::derive(cx, move || sp.clone());
-                    Some(view! { cx, <Page page /> })
-                }}</Suspense>
+        <div class="h-screen flex flex-col flex-wrap gap-2">
+            <div class="flex gap-2 p-2 w-full">
+                <For
+                    each=move || pages.read(cx).unwrap_or_default()
+                    key=|page| page.id.clone()
+                    view=move |cx, page| {
+                        let id = store_value(cx, page.id.clone());
+                        let is_selected = Signal::derive(cx, move || {
+                            selected_page().is_some_and(|sp| sp.id == id())
+                        });
+                        let select = SignalSetter::map(cx, move |p| set_selected_page(Some(p)));
+                        view! { cx, <PageTab
+                            page
+                            is_selected
+                            select
+                            submit_page
+                            delete_page
+                        /> }
+                    }
+                />
+                <FlexSpace />
+                <IconButton on:click=move |_| {
+                    submit_page.dispatch(PageType {
+                        id: "".into(),
+                        name: "new page".into(),
+                    })
+                }>
+                    <PlusIcon />
+                </IconButton>
+                <IconButton on:click=move |_| auth.logout() >
+                    <ArrowRightOnRectangleIcon />
+                </IconButton>
             </div>
-        </Suspense>
+            <Suspense fallback=move || "">{ move || {
+                let sp = selected_page()?;
+                let page = Signal::derive(cx, move || sp.clone());
+                Some(view! { cx, <Page page /> })
+            }}</Suspense>
+        </div>
     }
 }
