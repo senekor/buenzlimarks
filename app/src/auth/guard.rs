9fc2e6202b00bfa4d1186560516bb5ad1f81ca28
Remo Senekowitsch <remo@buenzli.dev>
2023-07-12 01:55:15 +0200

    fix navigation panic

--- app/src/auth/guard.rs
+++ app/src/auth/guard.rs
@@ -4,19 +4,25 @@ use leptos_router::use_navigate;
 use crate::{api::create_settings_resource, auth::use_token};
 
 pub fn create_auth_guard(cx: Scope) {
-    let navigate = use_navigate(cx);
     let token = use_token(cx);
     create_effect(cx, move |_| {
         if token().into_inner().is_none() {
-            navigate("/login", Default::default()).unwrap();
+            let navigate = use_navigate(cx);
+            // workaround for navigating during initial routing
+            // https://docs.rs/leptos_router/0.4.2/leptos_router/fn.use_navigate.html#panics
+            request_animation_frame(move || {
+                navigate("/login", Default::default()).unwrap();
+            });
         }
     });
-    let navigate = use_navigate(cx);
     let settings = create_settings_resource(cx);
     create_effect(cx, move |_| {
         if let Some(Err(_)) = settings.read(cx) {
+            let navigate = use_navigate(cx);
             // TODO handle other errors than "unauthenticated"
-            navigate("/login", Default::default()).unwrap();
+            request_animation_frame(move || {
+                navigate("/login", Default::default()).unwrap();
+            });
         }
     });
 }
