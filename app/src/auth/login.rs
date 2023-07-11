675abcc580ae417d8e7b8d814326764f7444a96c
Remo Senekowitsch <remo@buenzli.dev>
2023-07-11 23:33:07 +0200

    fix github login

--- app/src/auth/login.rs
+++ app/src/auth/login.rs
@@ -39,6 +39,7 @@ fn GithubLogin(cx: Scope) -> impl IntoView {
         <a
           class="bg-slate-600 w-fit rounded px-4 py-2 text-3xl"
           href="/api/auth/github/login"
+          rel="external" // make sure leptos doesn't use client-side routing
         >
           "GitHub Login"
         </a>
